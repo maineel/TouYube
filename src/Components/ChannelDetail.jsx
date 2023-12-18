8e78da3df4bb7c39139fc1edc6557db5533021d8
neel <neelxdxd@gmail.com>
2023-12-18 15:25:39 +0530

    Search functionality & channel page updated

--- src/Components/ChannelDetail.jsx
+++ src/Components/ChannelDetail.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import {Videos, ChannelCard} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail,setChannelDetail] = useState(null);
+  const [videos,setVideos] = useState([]);
+  const { id } = useParams();
+  
+  // console.log(videos)
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then((data) => setVideos(data?.items))
+   }, [id]);
+
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={{background: 'radial-gradient(circle, rgba(211,16,16,1) 0%, rgba(37,31,31,1) 50%, rgba(0,0,0,1) 100%)' , aIndex:10, height:'300px'}}/>
+        <ChannelCard channelDetail={channelDetail} marginTop='-93px'/>
+      </Box>
+      <Box display='flex' p='2'>
+        <Box sx={{mr:{sm:'100px'}}}/>
+        <Videos videos={videos}/>
+      </Box>
+    </Box>
   )
 }
 
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -5,17 +5,24 @@ import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
-    function handleSubmit() {
+    const [searchTerm,setSearchterm] = useState('');
+    const navigate = useNavigate();
 
-    }
-
-    function handleChange(){
-
+    function handleSubmit(event) {
+        event.preventDefault();
+        if(searchTerm){
+            navigate(`/search/${searchTerm}`);
+            setSearchterm('');
+        }
     }
     
+    function handleChange(event){
+        setSearchterm(event.target.value);
+    }
+
     return (
         <Paper component="form" onSubmit={handleSubmit} sx={{ borderRadius: 20, border:'1px solid #e3e3e3', pl:2, boxShadow:'none', mr:{sm:5}}}>
-            <input className='search-bar' placeholder='Search...' value="" onChange={handleChange} />
+            <input className='search-bar' placeholder='Search...' value={searchTerm} onChange={handleChange} />
             <IconButton type='submit' sx={{p:'10px', color:'red'}}>
                 <Search/>
             </IconButton>
